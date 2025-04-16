457f479c35c536e62f86dd38337ee1d3487d3b9e
JOOYUNHAK <wndbsgkr@naver.com>
2025-04-17 00:03:42 +0900

    add(libs): Time 인터페이스 요일 메서드 추가

--- libs/common/time/time.interface.ts
+++ libs/common/time/time.interface.ts
@@ -1,9 +1,19 @@
 export interface Time {
-  now(): Date;
+  getDayOfWeek(): number;
 }
 
 export class LocalTime implements Time {
-  now(): Date {
+  private _date: Date;
+
+  constructor(date: Date) {
+    this._date = date;
+  }
+
+  static now(): Date {
     return new Date();
+  }
+
+  getDayOfWeek(): number {
+    return this._date.getDay();
   }
 }
